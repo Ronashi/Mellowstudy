f6daf474236704ee37d091d3142701c426182da9
Reagan Ronald <reaganronashi@gmail.com>
2023-03-09 23:05:46 +0300

    paths

--- base/views.py
+++ base/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render
 
 
 rooms = [
-    # {'id', 'name':'Calculus made easy'},
+    
     {'id':1, 'name':'Engineers on board'},
     {'id':2, 'name':'Technical geeks'},
     {'id':3,  'name':'Lets program'},
 ]
 
 
-def home(request):
-    return  render(request, 'home.html')
+# def home(request):
+#     return  render(request, 'home.html', {'rooms': rooms})
 
-def room(request):
+
+def home(request):
+    context = {'rooms':rooms}
+    return  render(request, 'base/home.html', context)
+
+def room(request,pk):
     return render(request, 'room.html')   
+
 
 
 
