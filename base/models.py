f01ab649de64a021f9e60ad83e7474cc6cdad5db
Reagan Ronald <reaganronashi@gmail.com>
2023-03-25 14:30:28 +0300

    database and migrates

--- base/models.py
+++ base/models.py
@@ -7,5 +7,10 @@ class Room(models.Model):
     #topic
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    # participants = models.
     updated = models.DateTimeField(auto_now =True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
